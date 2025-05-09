9ce317888bc8826da7f5a453b7fa81d9fcc5d84a
Elfouih <ilyass.elfouih@gmail.com>
2025-05-09 16:39:30 +0100

    wip: build api

--- libs/api/sys-admin/auth/src/lib/sys-admin-auth-api.module.ts
+++ libs/api/sys-admin/auth/src/lib/sys-admin-auth-api.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { SysAdminAuthApiController } from './sys-admin-auth-api.controller';
 import { SysAdminAuthApiService } from './sys-admin-auth-api.service';
+import { CommonApiDbModule } from '@common/api/db';
 
 @Module({
   controllers: [SysAdminAuthApiController],
   providers: [SysAdminAuthApiService],
+  imports: [CommonApiDbModule],
   exports: [SysAdminAuthApiService],
 })
 export class SysAdminAuthApiModule {}
--- libs/common/api/db/src/lib/common-api-db.service.ts
+++ libs/common/api/db/src/lib/common-api-db.service.ts
@@ -1,34 +1,24 @@
 import { Injectable } from '@nestjs/common';
 import { SysAdminModel } from '@common/models';
-import * as sysAdminsMocks from './mocks/sys-admins.json';
 
 @Injectable()
 export class CommonApiDbService {
-  private sysAdmins: SysAdminModel[] = [...sysAdminsMocks];
-
   getSysAdminByName(name: string): SysAdminModel | undefined {
-    return this.sysAdmins.find((admin) => admin.name === name);
+    throw new Error('Method not implemented.');
   }
 
   createSysAdmin(sysAdmin: SysAdminModel): SysAdminModel {
-    this.sysAdmins.push(sysAdmin);
-    return sysAdmin;
+    throw new Error('Method not implemented.');
   }
 
   updateSysAdmin(
     name: string,
     updates: Partial<SysAdminModel>,
   ): SysAdminModel | undefined {
-    const index = this.sysAdmins.findIndex((admin) => admin.name === name);
-    if (index === -1) return undefined;
-
-    this.sysAdmins[index] = { ...this.sysAdmins[index], ...updates };
-    return this.sysAdmins[index];
+    throw new Error('Method not implemented.');
   }
 
   deleteSysAdmin(name: string): boolean {
-    const initialLength = this.sysAdmins.length;
-    this.sysAdmins = this.sysAdmins.filter((admin) => admin.name !== name);
-    return this.sysAdmins.length !== initialLength;
+    throw new Error('Method not implemented.');
   }
 }
