ce3469c3b06f05c0294bead661728b563efd1761
Jordan <jordan@jpdesigning.com>
2025-05-14 14:57:09 -0400

    feat: add conformance and CI check (#3)
    * feat: add conformance and CI checks
    
    * fix: update CI workflow
    
    * test without colon
    
    * fix ci
    
    * prefix with npx
    
    * update triggers
    
    * update formatting and add verbose logging
    
    * use env var
    
    * dedupe
    
    * add verbose logging
    
    * patch nx conformance v2
    
    * pin conformance
    
    * update nx
    
    * echo variable
    
    * mask
    
    * skip formatting
    
    * remove all conformance checks
    
    * add trace logging
    
    * update .gitignore and .vscode/settings.json to include new rules and instructions
    
    * remove echo and revert conformance checks
    
    * fix: pipeline unit test complains
    
    * fix: update commands in CI workflow to use npx for sync and conformance checks
    
    * feat: add references to TypeScript configuration for various apps
    
    * revert sync
    
    ---------
    
    Co-authored-by: Elfouih <ilyass.elfouih@gmail.com>

--- libs/api/sys-admin/auth/src/lib/sys-admin-auth-api.controller.spec.ts
+++ libs/api/sys-admin/auth/src/lib/sys-admin-auth-api.controller.spec.ts
@@ -1,13 +1,14 @@
 import { Test } from '@nestjs/testing';
 import { SysAdminAuthApiController } from './sys-admin-auth-api.controller';
 import { SysAdminAuthApiService } from './sys-admin-auth-api.service';
+import { CommonApiDbModule, CommonApiDbService } from '@common/api/db';
 
 describe('SysAdminAuthApiController', () => {
   let controller: SysAdminAuthApiController;
 
   beforeEach(async () => {
     const module = await Test.createTestingModule({
-      providers: [SysAdminAuthApiService],
+      providers: [SysAdminAuthApiService, CommonApiDbService],
       controllers: [SysAdminAuthApiController],
     }).compile();
 
--- libs/api/sys-admin/auth/src/lib/sys-admin-auth-api.service.spec.ts
+++ libs/api/sys-admin/auth/src/lib/sys-admin-auth-api.service.spec.ts
@@ -1,12 +1,13 @@
 import { Test } from '@nestjs/testing';
 import { SysAdminAuthApiService } from './sys-admin-auth-api.service';
+import { CommonApiDbService } from '@common/api/db';
 
 describe('SysAdminAuthApiService', () => {
   let service: SysAdminAuthApiService;
 
   beforeEach(async () => {
     const module = await Test.createTestingModule({
-      providers: [SysAdminAuthApiService],
+      providers: [SysAdminAuthApiService, CommonApiDbService],
     }).compile();
 
     service = module.get(SysAdminAuthApiService);
