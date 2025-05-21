daec31ae0546ec2385b9f5148150c09cfc15a376
Jordan Powell <jordan@jpdesigning.com>
2025-05-21 16:04:29 -0400

    e2e(pmo): add pmo e2e tests

---
+++ apps/pmo/e2e/example.spec.ts
@@ -0,0 +1,21 @@
+import { test, expect } from '@playwright/test';
+import { AppRouteTypes } from '../src/app/app.routes';
+
+test('has title', async ({ page }) => {
+  await page.goto('/');
+
+  // Expect h1 to contain a substring.
+  expect(await page.locator('p').innerText()).toContain('PmoHome works!');
+});
+
+test('can navigate to restaurants list', async ({ page }) => {
+  await page.goto(AppRouteTypes.RESTAURANTS_LIST);
+
+  // Expect the URL to contain the path for the restaurants list.
+  await expect(page).toHaveURL(/.*restaurants-list/);
+
+  // Expect h1 to contain a substring.
+  expect(await page.locator('p').innerText()).toContain(
+    'PmoRestaurantsList works!'
+  );
+})
