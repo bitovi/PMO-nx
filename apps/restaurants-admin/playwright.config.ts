48e88b9d1a10ee54bede8d085320699eb1a9bfea
Elfouih <ilyass.elfouih@gmail.com>
2025-05-27 00:20:07 +0100

    feat: update Playwright configuration to conditionally reuse existing server and remove unused browser projects

--- apps/restaurants-admin/playwright.config.ts
+++ apps/restaurants-admin/playwright.config.ts
@@ -26,7 +26,7 @@ export default defineConfig({
   webServer: {
     command: 'npx nx serve restaurants-admin',
     url: 'http://localhost:4200',
-    reuseExistingServer: true,
+    reuseExistingServer: !process.env.CI,
     cwd: workspaceRoot,
   },
   projects: [
@@ -34,35 +34,5 @@ export default defineConfig({
       name: 'chromium',
       use: { ...devices['Desktop Chrome'] },
     },
-
-    {
-      name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
-    },
-
-    {
-      name: 'webkit',
-      use: { ...devices['Desktop Safari'] },
-    },
-
-    // Uncomment for mobile browsers support
-    /* {
-      name: 'Mobile Chrome',
-      use: { ...devices['Pixel 5'] },
-    },
-    {
-      name: 'Mobile Safari',
-      use: { ...devices['iPhone 12'] },
-    }, */
-
-    // Uncomment for branded browsers
-    /* {
-      name: 'Microsoft Edge',
-      use: { ...devices['Desktop Edge'], channel: 'msedge' },
-    },
-    {
-      name: 'Google Chrome',
-      use: { ...devices['Desktop Chrome'], channel: 'chrome' },
-    } */
   ],
 });
