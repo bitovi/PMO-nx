0acdf4d3ee93bca3afd3e1739f7ba1b0e90354d8
Jordan Powell <jordan@jpdesigning.com>
2025-05-28 12:03:54 -0400

    remove failing demo spec

--- apps/sys-admin/e2e/demo.spec.ts
+++
@@ -1,8 +0,0 @@
-import { test, expect } from '@playwright/test';
-import { testUtil } from '../src/app/test-util';
-
-test('demo', async ({ page }) => {
-  await page.goto('/');
-
-  expect(1).toBe(testUtil());
-});
